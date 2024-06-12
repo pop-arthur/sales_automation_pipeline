5da921c268376ad79937b7899c545c2da4b75ad0
InnoNodo <k.nosov@innopolis.university>
2024-06-12 12:09:03 +0300

    Change way to the files, add shopping cart JS feature

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -14,3 +14,24 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+let container_for_search = [{ID: 1234, name: "Kirill", description: "HUI", price: "300$", amount: 3}];
+
+let item_block = document.getElementById("items-block");
+
+container_for_search.forEach(item => {
+    let newItem = document.createElement('div');
+    newItem.classList.add('item-shopping-cart');
+
+    newItem.innerHTML = `
+        <p>ID: ${item.ID} Name: ${item.name} 
+        Description: ${item.description} Price: ${item.price} Amount: ${item.amount}</p>
+    `;
+
+    item_block.appendChild(newItem);
+});
+
+let container_for_cart = [{}]
+
+let shopping_cart = document.getElementById("shopping-cart-items")
+
